fb63edb85a78bc7a82ba1bc754bef2e43ec86e9c
Azazel0203 <139248351+Azazel0203@users.noreply.github.com>
2024-02-02 21:39:53 +0530

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -18,7 +18,7 @@ dropout = 0.2
 
 torch.manual_seed(1337)
 
-# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
